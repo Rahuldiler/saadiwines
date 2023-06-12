8dd3520d41fa759c9d157963ef8bcb76b939f55a
vibhorg <vibhorg@adobe.com>
2023-06-12 22:58:03 +0530

    Fixes

--- src/components/common/LoginModal.jsx
+++ src/components/common/LoginModal.jsx
@@ -1,14 +1,14 @@
 import { Box, Dialog } from "@mui/material";
 import React from "react";
-import SignUp from "../auth/Sign-up";
-import Login from "../auth/Login";
+import SignUp from "@/Components/my-account/Sign-up";
+import SignIn from "@/Components/my-account/Login";
 
 function LoginModal({ open, handle, handleClose, setHandle }) {
   return (
     <Dialog open={open} onClose={handleClose}>
       <Box sx={{ p: 4, background: "#fff" }}>
         {handle ? (
-          <Login setHandle={setHandle} />
+          <SignIn setHandle={setHandle} />
         ) : (
           <SignUp setHandle={setHandle} />
         )}
