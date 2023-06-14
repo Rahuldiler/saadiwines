5844dac3e0d43848612309c4ac3b4f2fec57efe5
vibhorg <vibhorg@adobe.com>
2023-06-14 23:56:16 +0530

    handling for cors

--- src/services/HTTPClientHandler.js
+++ src/services/HTTPClientHandler.js
@@ -8,17 +8,22 @@ export default class HTTPClientHandler {
         } else {
             this.token = null;
         }
+        this.headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
 
     }
 
     get = async (options) => {
-        let headers = options.headers;
+        let headers = {...options.headers, ...this.headers};
         headers = options.isSecured ? {...headers, Authorization: `Bearer ${this.token}`} : headers;
         return await axios.get(options.url, {params: options.params, headers});
     }
 
     post = async (options) => {
-        let headers = options.headers;
+        let headers = {...options.headers, ...this.headers};
         headers = options.isSecured ? {...headers, Authorization: `Bearer ${this.token}`} : headers;
         return await axios.post(
             options.url,
@@ -30,7 +35,7 @@ export default class HTTPClientHandler {
         )
     }
     put = async (options) => {
-        let headers = options.headers;
+        let headers = {...options.headers, ...this.headers};
         headers = options.isSecured ? {...headers, Authorization: `Bearer ${this.token}`} : headers;
         return await axios.put(options.url,
             options.payload,
@@ -41,7 +46,7 @@ export default class HTTPClientHandler {
         )
     }
     delete = async (options) => {
-        let headers = options.headers;
+        let headers = {...options.headers, ...this.headers};
         headers = options.isSecured ? {...headers, Authorization: `Bearer ${this.token}`} : headers;
         return await axios.post(options.url,
             {
