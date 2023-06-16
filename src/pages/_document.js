739cded085eb99fcbf373bbd4908ad22e314e8bf
admin-sv <136553422+admin-sv@users.noreply.github.com>
2023-06-16 13:18:06 +0530

    added pintrest meta and favicon (#24)
    Co-authored-by: vibhorg <vibhorg@adobe.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="p:domain_verify" content="16cb29b8246289202 a47eea747bf917"/>
+                <title>Shaadi Vines</title>
+                <link rel="icon" href="/favicon.png" type="image/png"/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
