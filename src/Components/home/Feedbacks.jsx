0e5057aa0af87dec482a5832bcee8e93454246e0
vibhorg <vibhorg@adobe.com>
2023-07-06 21:25:15 +0530

    Minor text updates

--- src/Components/home/Feedbacks.jsx
+++ src/Components/home/Feedbacks.jsx
@@ -26,9 +26,9 @@ function Feedbacks() {
       image: "/assets/SanjanaRawat.png",
     },
     {
-      name: "Raj Mathew",
+      name: "Sanjay Malhotra",
       description:
-        "Shaddivines is a great platform for wedding planning. Its a one stop solution for all your wedding needs. I would highly recommend it to everyone.",
+        "Shaadivines is a great platform for wedding websites. Its a one stop solution for all your wedding needs. I would highly recommend it to everyone.",
       image: "/assets/raj-mathew.png",
     },
     {
