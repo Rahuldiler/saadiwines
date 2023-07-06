6d8d7b183c068d048f84b18bbf65a2a793dd9702
vibhorg <vibhorg@adobe.com>
2023-07-06 13:50:58 +0530

    Fixes

--- src/Components/all-templates/TemplateOne.jsx
+++ src/Components/all-templates/TemplateOne.jsx
@@ -10,9 +10,9 @@ import ContactForm from "@/Components/template-components/1/rsvpForm";
 import Lightbox from "@/Components/template-components/1/lightBox";
 import Cards from "@/Components/template-components/1/Cards";
 import { useEffect } from "react";
-import { templateInfoData } from "@/constants/templateInfo";
 import Steps from "@/Components/template-components/1/steps";
 import { useRouter } from "next/router";
+import {templateInfoData} from "@/constants/1";
 
 function TemplateOne({ formData, staticTemplateData }) {
   const [currentSlide, setCurrentSlide] = useState(0);
