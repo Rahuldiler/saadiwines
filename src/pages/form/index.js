481316e6e272ba8495e5c8e53b1818427ac1c133
vibhorg <vibhorg@adobe.com>
2023-07-12 20:39:05 +0530

    Added cache control

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -103,7 +103,9 @@ function index() {
 
   const getBase64FromUrl = async (url) => {
     if (url) {
-      const data = await fetch(url);
+      const data = await fetch(url, {
+        headers: { "Cache-Control": "no-cache" },
+      });
       const blob = await data.blob();
       return new Promise((resolve) => {
         const reader = new FileReader();
